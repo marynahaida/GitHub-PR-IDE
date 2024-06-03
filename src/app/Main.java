7c663ab4cecdb0c69ec2dac6356e551072ebbfd2
marynahaida <marinkagaida.15@gmail.com>
2024-06-03 20:14:19 +0200

    Update app info

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,8 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("My Git app");
+        System.out.println("My git app. Super!");
     }
 }
